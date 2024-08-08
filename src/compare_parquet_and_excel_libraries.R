4c4fbcd5a8af24b745529c0187b2b728e1f577e9
Seyed Reza Mirkhani <seyedrezamirkhani@yahoo.co.uk>
2024-08-08 19:40:08 +0100

    updated to import functions from common_functions.R

--- src/compare_parquet_and_excel_libraries.R
+++ src/compare_parquet_and_excel_libraries.R
@@ -9,6 +9,8 @@ library(openxlsx)
 library(writexl)
 library(arrow, warn.conflicts = FALSE)
 
+source("common_functions.R")
+
 test_result_file_name <- "../optimisation_data/parquet_and_excel_write_result.csv"
 
 input_folder <- "../output_data/"
@@ -29,52 +31,10 @@ test_file_names <- c(
   "UnlabeledSampleScore-300-features.parquet"
 )
 
-LoadData <- function(file_name) {
-  full_file_name <- paste(input_folder, file_name, sep = "")
-
-  df <- read_parquet(full_file_name)
-
-  return(df)
-}
-
-WriteAndTimeOpenXLSX <- function(df) {
-  full_file_name <- tempfile(pattern = "file", fileext = ".xslt")
-
-  start_time <- Sys.time()
-  df <- write.xlsx(df, file = full_file_name, colNames = FALSE)
-  end_time <- Sys.time()
-
-  duration <- as.numeric(difftime(end_time, start_time), units = "secs")
-
-  return(duration)
-}
-
-WriteAndTimeWriteXL <- function(df) {
-  full_file_name <- tempfile(pattern = "file", fileext = ".xslt")
-
-  start_time <- Sys.time()
-  df <- write_xlsx(df, full_file_name, format_headers = FALSE, use_zip64 = TRUE)
-  end_time <- Sys.time()
-
-  duration <- as.numeric(difftime(end_time, start_time), units = "secs")
-
-  return(duration)
-}
-
-WriteAndTimeArrow <- function(df) {
-  full_file_name <- tempfile(pattern = "file", fileext = ".parquet")
-
-  start_time <- Sys.time()
-  write_parquet(df, full_file_name)
-  end_time <- Sys.time()
-
-  duration <- as.numeric(difftime(end_time, start_time), units = "secs")
-
-  return(duration)
-}
+full_test_file_names <- unlist(lapply(test_file_names, function(x) paste(input_folder, x, sep = "")))
 
 
-dfs <- sapply(test_file_names, LoadData)
+dfs <- sapply(full_test_file_names, ReadParquetData)
 nrows <- unlist(sapply(dfs, nrow, USE.NAMES = FALSE))
 ncols <- unlist(sapply(dfs, ncol, USE.NAMES = FALSE))
 dfsize <- unlist(sapply(dfs, object.size, USE.NAMES = FALSE))
